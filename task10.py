d4d392efe0ee41cf9bd538046781c04be1fcbece
James Busari <j.busari1@universityofgalway.ie>
2024-10-09 16:54:52 +0100

    All

--- task10.py
+++ task10.py
@@ -1,3 +1,21 @@
 import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
+import pandas as pd
+
+data_f = pd.read_csv('task9.csv')
+
+data_f.set_index('Date', inplace=True)
+
+plt.figure(figsize=(12,6))
+for company in data_f.columns:
+    plt.plot(data_f.index, data_f[company], label = company)
+
+plt.title('Stock price Over Time')
+plt.xlabel('Date')
+plt.ylabel('Stock price')
+plt.legend()
+plt.xticks(rotation = 45)
+plt.grid()
+plt.tight_layout()
+plt.show()
