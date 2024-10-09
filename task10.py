891f49a27db3bbd29e5e28af4b8ed11529fe2c8b
James Busari <j.busari1@universityofgalway.ie>
2024-10-09 16:58:39 +0100

    d

--- task10.py
+++ task10.py
@@ -9,7 +9,9 @@ data_f.set_index('Date', inplace=True)
 
 plt.figure(figsize=(12,6))
 for company in data_f.columns:
-    plt.plot(data_f.index, data_f[company], label = company)
+    initial = data_f[company].iloc[0]
+    percentage_change = ((data_f[company].iloc[-1] - initial)/initial )* 100
+    plt.plot(data_f.index, data_f[company], label = f'{company} ({percentage_change: .2f}%)')
 
 plt.title('Stock price Over Time')
 plt.xlabel('Date')
