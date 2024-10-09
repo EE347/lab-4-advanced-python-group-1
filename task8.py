b0305d279793497bd08b74b61e4edfdaaa670250
James Busari <j.busari1@universityofgalway.ie>
2024-10-09 16:48:10 +0100

    Nearly done

--- task8.py
+++ task8.py
@@ -1,13 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open("task_7_cos.npy", "r") as f1:
-    data1 = f1.read()
-    f1.close()
-
-with open("task_7_sin.npy", "r") as f2:
-    data2 = f2.read()
-f2.close()
+data1 = np.load("task_7_cos.npy")
+data2 = np.load("task_7_sin.npy")
 
 plt.plot(data1)
 plt.plot(data2)
+
+plt.show()
