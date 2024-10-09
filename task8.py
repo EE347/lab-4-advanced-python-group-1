960b50afa42d6b15c5408a7cbe5728498eccb37c
James Busari <j.busari1@universityofgalway.ie>
2024-10-09 16:50:59 +0100

    Lab

--- task8.py
+++ task8.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 data1 = np.load("task_7_cos.npy")
 data2 = np.load("task_7_sin.npy")
 
+plt.title('Sin vs Cos')
+plt.xlabel('x vals')
+plt.ylabel('y vals')
 plt.plot(data1)
 plt.plot(data2)
-
+plt.legend()
 plt.show()
